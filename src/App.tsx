96b2efbff14c5223e19bc6664e649cc09d543a3e
vtwqajjq <vitywqajjq@icloud.com>
2022-07-24 11:07:43 +0300

    Change README.md file

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import Display from "./Components/Display/Display";
+import Button from "./Components/Button/Button";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const minValue = 0
+    const maxValue = 5
+
+    let [counter, setCounter] = useState(minValue)
+    const incrementHandler = () => {
+        setCounter(counter + 1)
+    }
+
+    const resetHandler = () => {
+        if (counter === maxValue) setCounter(minValue)
+    }
+
+    const conditionInc = counter === maxValue
+    const conditionReset = counter === minValue
+
+    return (
+        <div className="App">
+            <div className='wrapper'>
+                <Display counter={counter}/>
+                <div className='buttons'>
+                    <Button condition={conditionInc} name='inc' handler={incrementHandler}/>
+                    <Button condition={conditionReset} name='reset' handler={resetHandler}/>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
